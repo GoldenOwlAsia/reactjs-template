c0f05fa898dfba46a76c1479311e85ae7c1cea68
Truong <truongprobk1992@gmail.com>
2019-01-14 17:30:52 +0700

    react-helmet

--- src/screen/About/index.js
+++ src/screen/About/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import './style.css';
 
 export default () => (
   <div className="about-container">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>About</title>
+    </Helmet>
     <span className="about-title">About Screen</span>
   </div>
 );
--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import './style.css';
 
 export default () => (
   <div className="home-container">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Home</title>
+    </Helmet>
     <span className="home-title">Home Screen</span>
   </div>
 );
